5d2cc901165bf8db5efe8bd004a03528b45ec9bf
David Stearns <knavedavid@gmail.com>
2026-09-03 07:06:13 -0400

    Cleanup a bit

--- src/main.lua
+++ src/main.lua
@@ -21,16 +21,22 @@
 --[[$const]] PLAYER_DYING = 6
 
 --player
-p1=
+local p1=
 {
 	x=71,
 	y=16,
 	state=0,
 	facing_left=false,
-	pos = function()
-		return {x=x/8,y=y/8}
-	end
 }
+
+function p1:pos()
+	return {x=self.x/8, y=self.y/8}
+end
+
+function round(value)
+	return value >= 0 and flr(value + 0.5) or ceil(value - 0.5)
+end
+
 gold = 0
 
 function place_player()
@@ -61,87 +67,48 @@ function _init()
 	count_remaining_gold()
 end
 
--- function collide_map(sprite)
--- 	local colliding = false
--- 	local x1 = sprite.x/8 -- leftish x
--- 	local x2 = (sprite.x+7)/8 -- rightish x
--- 	local y1 = sprite.y/8 -- uppish y
--- 	local y2 = (sprite.y+7)/8 -- downish y
--- 	local a = fget(mget(x1,y1),COLLISION_FLAG)
--- 	local b = fget(mget(x1,y2),COLLISION_FLAG)
--- 	local c = fget(mget(x2,y1),COLLISION_FLAG)
--- 	local d = fget(mget(x2,y2),COLLISION_FLAG)
--- 	return a or b or c or d
--- end
-
--- function touching(sprite, thing_to_check)
--- 	-- sprite could be overlapping as many as four tiles. We need to get all of them.
--- 	local x_l = sprite.x/8 -- leftish x
--- 	local x_r = (sprite.x+7)/8 -- rightish x
--- 	local y_u = sprite.y/8 -- uppish y
--- 	local y_d = (sprite.y+7)/8 -- downish y
--- 	local a = mget(x_l,y_u) == thing_to_check
--- 	local b = mget(x_l,y_d) == thing_to_check
--- 	local c = mget(x_r,y_u) == thing_to_check
--- 	local d = mget(x_r,y_d) == thing_to_check
--- 	return a or b or c or d
--- end
-
-function get_tile(x,y)
-	local x = flr(x/8+0.5)
-	local y = flr(y/8+0.5)
-	return mget(x,y)
+function get_tile(pos)
+	return mget(
+		round(pos.x),
+		round(pos.y)
+	)
 end
 
-
-function near(sprite, thing_to_check)
-	return get_tile(sprite.x,sprite.y) == thing_to_check
+function get_floor(pos)
+	return mget(
+		round(pos.x),
+		pos.y+1
+	)
 end
 
-function near_flag(sprite, flag_to_check)
-	return fget(get_tile(sprite.x,sprite.y),flag_to_check)
+function get_ceiling(pos)
+	return mget(
+		round(pos.x),
+		ceil(pos.y-1)
+	)
 end
 
-function get_floor(x,y)
-	local x = flr((x/8)+0.5)
-	local y = (y+8)/8
-	return mget(x,y)
+function get_left(pos)
+	return mget(
+		ceil(pos.x-1),
+		round(pos.y)
+	)
 end
 
-function get_ceiling(x,y)
-	local x = flr((x/8)+0.5)
-	local y = ceil((y-8)/8)
-	return mget(x,y)
+function get_right(pos)
+	return mget(
+		pos.x+1,
+		round(pos.y)
+	)
 end
-
-function get_left(x,y)
-	local tile_x = ceil((x-8)/8)
-	local tile_y = flr((y/8)+0.5)
-	return mget(tile_x,tile_y)
-end
-
-function get_right(x,y)
-	local tile_x = (x+8)/8
-	local tile_y = flr((y/8)+0.5)
-	return mget(tile_x,tile_y)
-end
-
-function near_under(sprite, thing_to_check)
-	return get_floor(sprite.x,sprite.y) == thing_to_check
-end
-
-function near_under_flag(sprite,flag_to_check)
-	return fget(get_floor(sprite.x,sprite.y), flag_to_check)
-end
-
 
 function win()
 	printh("You win")
 end
 
-function get_gold(x,y)
+function get_gold(pos)
 	sfx(GOLD_SOUND)
-	mset(x,y,0)
+	mset(round(pos.x),round(pos.y),0)
 	gold -= 1
 	if gold <= 0 then
 		win()
@@ -153,37 +120,37 @@ function shoot()
 end
 
 function _update()
-	-- Gathering information
-	touching_ladder = near(p1, LADDER_TILE) or near_under(p1, LADDER_TILE)
-
 	-- Figure out player's surroundings
-	local down_allowed = not near_under_flag(p1, COLLISION_FLAG)
+	local floor = get_floor(p1:pos())
+	local grounded = fget(floor, COLLISION_FLAG)
+	local player_tile = get_tile(p1:pos())
+	local touching_ladder = player_tile == LADDER_TILE or floor == LADDER_TILE
+
+	local down_allowed = not grounded
 	
-	local grounded = false
-	if near_under(p1, LADDER_TILE) or near_under_flag(p1, COLLISION_FLAG) then
+	if floor == LADDER_TILE or grounded then
 		p1.state = PLAYER_STANDING
-		grounded = true
 	else
 		p1.state = PLAYER_FALLING
 	end
 
 	local up_allowed = false
 	if touching_ladder then
-		if not near_under_flag(p1, COLLISION_FLAG) then
+		if not grounded then
 			p1.state = PLAYER_CLIMBING
 		end
-		if not ((p1.y % 8 == 0) and not near(p1, LADDER_TILE)) then
+		if not ((p1.y % 8 == 0) and not (player_tile == LADDER_TILE)) then
 			up_allowed = true
 		else
 			p1.state = PLAYER_STANDING
 		end
 	end
-	if fget(get_ceiling(p1.x,p1.y),COLLISION_FLAG) then
+	if fget(get_ceiling(p1:pos()),COLLISION_FLAG) then
 		up_allowed = false
 	end
 
-	local left_allowed = not fget(get_left(p1.x, p1.y),COLLISION_FLAG)
-	local right_allowed = not fget(get_right(p1.x, p1.y),COLLISION_FLAG)
+	local left_allowed = not fget(get_left(p1:pos()),COLLISION_FLAG)
+	local right_allowed = not fget(get_right(p1:pos()),COLLISION_FLAG)
 
 	-- Check Player input
 	local dx = 0
@@ -221,35 +188,27 @@ function _update()
 			dy = 0
 		end
 	end
-	-- printh(p1.state)
 
 	-- adjust movement
 	-- the game nudges the player towards the center of the axis they are walking on
-
 	
 	-- Resolve movement
-	local xoffset=0
-	local yoffset=0
-	if dx>0 then xoffset=7 end
-	if dy>0 then yoffset=7 end
-	local colliding_x = (p1.x+xoffset)/8
-	local colliding_y = (p1.y+yoffset)/8
-	local next_tile=mget(colliding_x,colliding_y)
 	p1.x += dx
+	p1.y += dy
 
 	if p1.state == PLAYER_FALLING then
 		p1.y+=GRAVITY
 	end
-	if p1.state == PLAYER_STANDING then
+	floor = get_floor(p1:pos())
+	grounded = fget(floor, COLLISION_FLAG)
+	if grounded then
 		p1.y = flr((p1.y)/8)*8
-	end
-	if (up_allowed or down_allowed) then
-		p1.y += dy
 	end
 
 	-- game logic
-	if near(p1, GOLD_TILE) then
-		get_gold(colliding_x,colliding_y)
+	local next_tile = get_tile(p1:pos())
+	if next_tile == GOLD_TILE then
+		get_gold(p1:pos())
 	end
 end
 
